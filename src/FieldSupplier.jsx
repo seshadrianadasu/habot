85ee7edc7215c10ddb32b43d6ada5188b90c7eae
Seshadri <seshadrianadasu@gmail.com>
2024-11-05 16:28:39 +0530

    Updates

--- src/FieldSupplier.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function FieldSupplier() {
-  return (
-    <div>FieldSupplier</div>
-  )
-}
-
-export default FieldSupplier
--- src/FieldTags.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function FieldTags() {
-  return (
-    <div>FieldTags</div>
-  )
-}
-
-export default FieldTags
